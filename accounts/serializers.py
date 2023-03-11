7e6096d46add0d292e2b4f0913e81d22ad966734
faisalt14 <faisalt14@gmail.com>
2023-03-11 18:12:07 -0500

    Completed CombinedListView for shopping cart

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,21 +1,28 @@
 from rest_framework import serializers
-from accounts.models import User
+from accounts.models import User, ShoppingRecipeModel
 from rest_framework.response import Response
+
 
 class UserDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'password', 'email', 'first_name', 'last_name', 'phone_num']
-    
+
     def create(self, validated_data):
-        user = User.objects.create_user(username=validated_data.get('username'), password=validated_data.get('password'), email=validated_data.get('email'), first_name=validated_data.get('first_name'), last_name=validated_data.get('last_name'), phone_num=validated_data.get('phone_num'))
+        user = User.objects.create_user(username=validated_data.get('username'),
+                                        password=validated_data.get('password'), email=validated_data.get('email'),
+                                        first_name=validated_data.get('first_name'),
+                                        last_name=validated_data.get('last_name'),
+                                        phone_num=validated_data.get('phone_num'))
         user.save()
         return Response({'message': 'success'}, status=200)
+
 
 class UserLoginSerializer(serializers.Serializer):
     class Meta:
         model = User
         fields = ['username', 'password']
+
 
 class UserEditSerializer(serializers.ModelSerializer):
     class Meta:
@@ -24,4 +31,9 @@ class UserEditSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'password': {'required': False}
         }
-        
+
+
+class AllShoppingListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShoppingRecipeModel
+        fields = ['user_id', 'recipe_id', 'servings_num']
--- accounts/views.py
+++ accounts/views.py
@@ -2,14 +2,18 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, logout as auth_logout, login as auth_login, update_session_auth_hash
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from accounts.models import User
-from accounts.serializers import UserDetailSerializer, UserLoginSerializer, UserEditSerializer
+from accounts.models import User, ShoppingRecipeModel
+from accounts.serializers import UserDetailSerializer, UserLoginSerializer, UserEditSerializer, \
+    AllShoppingListSerializer
 
 # Create your views here.
+from recipes.models import IngredientModel
+
+
 class SignUpView(CreateAPIView):
     def post(self, request):
         serializer = UserDetailSerializer(data=request.data)
@@ -18,13 +22,13 @@ class SignUpView(CreateAPIView):
                 try:
                     validate_email(request.data.get('email'))
                 except ValidationError:
-                        return Response({'message': 'enter a valid email'}, status=400)
+                    return Response({'message': 'enter a valid email'}, status=400)
             if request.data.get('password') != request.data.get('password2'):
                 return Response({'message': 'passwords do not match'}, status=400)
             serializer.create(request.data)
             return Response({'message': 'signup success'}, status=200)
-        
-        if User.objects.filter(username = request.data.get('username')).exists():
+
+        if User.objects.filter(username=request.data.get('username')).exists():
             return Response({'message': 'username is already taken'}, status=400)
         errors = ""
 
@@ -33,6 +37,7 @@ class SignUpView(CreateAPIView):
         for error in serializer.errors:
             errors = errors + ' [' + error + ' - ' + serializer.errors[error][0].title() + ']'
         return Response({'message': 'Errors in request:' + errors}, status=400)
+
 
 class LoginView(APIView):
     def post(self, request):
@@ -43,17 +48,20 @@ class LoginView(APIView):
                 return Response({'message': 'Username or password is invalid'}, status=400)
             auth_logout(request)
             auth_login(request, user)
-            return Response({'message': 'logged in as ' + user.username }, status=200)
+            return Response({'message': 'logged in as ' + user.username}, status=200)
         print(serializer.errors)
         return Response({'message': 'serializer is invalid!'}, status=400)
+
 
 class LogoutView(APIView):
     def get(self, request):
         auth_logout(request)
         return Response({'message': 'logged out'}, status=200)
 
+
 class EditProfileView(APIView):
     permission_classes = [IsAuthenticated]
+
     def post(self, request):
         if request.user.is_authenticated == False:
             return Response({'message': 'Not logged in'}, status=401)
@@ -66,7 +74,7 @@ class EditProfileView(APIView):
                     validate_email(request.data.get('email'))
                     request.user.email = request.data.get('email')
                 except ValidationError:
-                        return Response({'message': 'enter a valid email'}, status=400)
+                    return Response({'message': 'enter a valid email'}, status=400)
             if request.data.get('password') != request.data.get('password2'):
                 return Response({'message': 'passwords do not match'}, status=400)
             elif request.data.get('password'):
@@ -85,7 +93,7 @@ class EditProfileView(APIView):
 
             request.user.save()
             update_session_auth_hash(request, request.user)
-            return Response({'message': 'edited user '+ request.user.username + "'s information"}, status=200)
+            return Response({'message': 'edited user ' + request.user.username + "'s information"}, status=200)
         print(serializer.errors)
         Email_Error = ''
         try:
@@ -94,3 +102,53 @@ class EditProfileView(APIView):
         except:
             pass
         return Response({'message': 'serializer is invalid!' + Email_Error}, status=400)
+
+
+def ingredientExists(name: str, ingredients: list):
+    for each_dict in ingredients:
+        if each_dict['name'] == name:
+            return True
+    return False
+
+
+def updateQuantity(name: str, ingredients: list, quauntity: int):
+    for each_dict in ingredients:
+        if each_dict['name'] == name:
+            each_dict['quantity'] += quauntity
+    return ingredients
+
+
+class CombinedListView(APIView):
+    serializer_class = AllShoppingListSerializer
+
+    # def get_queryset(self):
+    #     return ShoppingRecipeModel.objects.filter(id=self.request.user.id)
+
+    def get(self, request, *args, **kwargs):
+        recipies_in_cart = ShoppingRecipeModel.objects.filter(user_id=self.request.user.id).values()
+        # print(recipies_in_cart)
+        # return Response(recipies_in_cart)
+
+        # loop over each dictionary item in recipies_in_cart to get recipe_id
+        ingredients = []
+        for i in range(0, len(recipies_in_cart)):
+            # get recipe_id_id
+            recipeID = recipies_in_cart[i]['recipe_id_id']
+            data = IngredientModel.objects.filter(recipe_id=recipeID).values()[0]
+            # print(data)
+
+            ingredient_name = data['name']
+            ingredient_quantity = data['quantity']
+
+            if ingredientExists(ingredient_name, ingredients):
+                ingredients = updateQuantity(ingredient_name, ingredients, ingredient_quantity)
+            else:
+                ingredients.append({
+                    'name': ingredient_name,
+                    'quantity': ingredient_quantity
+                })
+
+        # print(ingredients)
+        return Response(ingredients)
+
+
